a6a5acf76875f84f0d6ba16ce222813a8c4c0673
na.shornikov <nonickk9@gmail.com>
2021-12-28 19:32:26 +0300

    fix

--- main.py
+++ main.py
@@ -77,8 +77,8 @@ def find_timofey(message):
     bot.send_message(message.chat.id, "Производится поиск по запросу 'Тимофей Сафронов'")
 
     for i in range(100):
-        time.sleep(random.randint(5, 15))
-        if i < 10 or i % 5 == 0:
+        time.sleep(random.randint(3, 7))
+        if i < 60:
             bot.send_message(message.chat.id, f"{i}%")
 
     bot.send_message(message.chat.id, "ПИДОРАС НАЙДЕН: https://www.google.ru/maps/place/%D0%A8%D0%B0%D1%83%D1%80%D0%BC%D0%B0+Tbilisuri/@50.3987878,30.5297598,17.75z/data=!4m5!3m4!1s0x40d4cf92829a0ed3:0xc03f3131bc668a15!8m2!3d50.3988509!4d30.530498")
