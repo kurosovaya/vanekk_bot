d76d6c869e8aa3dbcd5cdf1d029708c5eb9bea2f
na.shornikov <nonickk9@gmail.com>
2022-01-04 23:27:03 +0300

    fix

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import telebot
+from telebot.types import Message
 
 from flask import Flask, request
 import os
 import time
 import markovify
-import random
 import super_secret_code
 
 TOKEN = os.environ["TOKEN"]
@@ -14,16 +14,15 @@ server = Flask(__name__)
 prohibited_stickers = ("AgADDgADxk5iIA",)
 bad_sticker = "CAACAgIAAxkBAANFYVighWF44_O1hkQlo_8QJSnYKJUAAg4AA8ZOYiAVktAJXEArfiEE"
 good_sticker = "CAACAgIAAxkBAANXYVi_oJqE_JrrKVh1fol8yID6CUgAAnIBAAI0-xcGv6CkXEKqmUQhBA"
-geo_search_active = False
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-with open("vanekkk_sentences.txt") as f:
+with open("vanekkk_sentences.txt", encoding="utf-8") as f:
     vanekk_messages = f.read()
-with open("pretentious_phrases.txt") as f:
+with open("pretentious_phrases.txt", encoding="utf-8") as f:
     fashion_phrases = f.read()
-with open("quotes_about_nationalism.txt") as f:
+with open("quotes_about_nationalism.txt", encoding="utf-8") as f:
     some_shit = f.read()
 # with open("Собачье сердце. Михаил Булгаков.txt") as f: # выходит полная хуйня
 #     classic = f.read()
@@ -57,17 +56,16 @@ def make_sentence(message):
 
 
 @bot.message_handler(content_types=["text"])
-def send_text(message):
+def send_text(message: Message):
     _a = {}
     exec(super_secret_code.something, _a)
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Всем привет с вами бендер")
-    elif message.text.lower() == "хочу сосать":
-        bot.send_sticker(message.chat.id, bad_sticker)
     elif message.text.lower() == "пока":
         bot.send_message(message.chat.id, "Какой же ты еблан")
     elif _a.popitem()[1](message.text):
         bot.delete_message(message.chat.id, message.message_id)
+        bot.send_message(f"{message.chat.first_name} -1 социальный кредит!")
 
 
 @bot.message_handler(content_types=["sticker"])
@@ -79,20 +77,24 @@ def send_sticker(message):
         bot.delete_message(sent_msg.result.chat.id, sent_msg.result.message_id)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "Йух", 200
-
-
-@server.route("/", methods=["GET"])
-def webhook():
+if os.environ["POLLING"]:
     bot.remove_webhook()
-    bot.set_webhook("https://vanekk-bot.herokuapp.com/" + TOKEN)
-    return "Хуй", 200
+    bot.infinity_polling()
+else:
+    @server.route('/' + TOKEN, methods=['POST'])
+    def get_message():
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return "Йух", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    @server.route("/", methods=["GET"])
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook("https://vanekk-bot.herokuapp.com/" + TOKEN)
+        return "Хуй", 200
+
+
+    if __name__ == "__main__":
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
